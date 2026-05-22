24d385eafbb0c6fa944f8dc310aa5a35b6c0419e
Gosia <gosia.wiejacha@cien.ai>
2026-05-22 15:05:51 +0000

    Revert "Add sync mappings new SDK client"
    This reverts commit ccf86be113dd0566e1ad050941bfb8aa28f60273.

--- clients/python/src/cien_agent_sdk/admin/__init__.py
+++ clients/python/src/cien_agent_sdk/admin/__init__.py
@@ -15,7 +15,6 @@ from .powerbi import AdminPowerBIAPI
 from .sync import AdminSyncAPI
 from .sync_live_query import AdminSyncLiveQueryAPI
 from .sync_mappings import AdminSyncMappingsAPI
-from .sync_mappings_new import AdminSyncMappingsNewAPI
 from .sync_source_definitions import AdminSyncSourceDefinitionsAPI
 
 
@@ -36,5 +35,4 @@ class AdminClient:
         self.sync = AdminSyncAPI(transport)
         self.sync_live_query = AdminSyncLiveQueryAPI(transport)
         self.sync_mappings = AdminSyncMappingsAPI(transport)
-        self.sync_mappings_new = AdminSyncMappingsNewAPI(transport)
         self.sync_source_definitions = AdminSyncSourceDefinitionsAPI(transport)
